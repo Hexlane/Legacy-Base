fe43aadc4e76d775a8d52ba61497cf8bec1ebbf6
Hexlane <deede3771@gmail.com>
2026-01-25 22:02:54 -0500

    Update script_global.cpp

--- src/game/gta/script_global.cpp
+++ src/game/gta/script_global.cpp
@@ -1,1 +1,10 @@
+#include "script_global.h"
+#include "game/pointers/Pointers.hpp"
 
+namespace NewBase
+{
+	void* script_global::get() const
+	{
+		return Pointers.m_script_globals[m_index >> 0x12 & 0x3F] + (m_index & 0x3FFFF);
+	}
+}
