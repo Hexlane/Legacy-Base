8ba3c8fbe5e1859fdda59908a7ed1b00c6b287d1
Hexlane <deede3771@gmail.com>
2026-01-25 20:08:29 -0500

    Add new function pointers and update PointerData

--- src/game/pointers/Pointers.hpp
+++ src/game/pointers/Pointers.hpp
@@ -1,14 +1,31 @@
 #pragma once
 #include <d3d11.h>
 #include <windows.h>
+#include "game/gta/fwddec.h"
+#include "game/gta/natives.h"
 
 namespace NewBase
 {
+namespace functions
+{
+	using get_native_handler_t = rage::scrNativeHandler(*)(rage::scrNativeRegistrationTable*, rage::scrNativeHash);
+	using fix_vectors_t = void(*)(rage::scrNativeCallContext*);
+	using run_script_threads_t = bool(*)(std::uint32_t ops_to_execute);
+}
+
+
 	struct PointerData
 	{
 		IDXGISwapChain** SwapChain;
 		HWND Hwnd;
 		WNDPROC WndProc;
+		CPedFactory **m_ped_factory{};
+		PVOID m_init_native_tables;
+		rage::scrNativeRegistrationTable *m_native_registration_table{};
+		functions::get_native_handler_t m_get_native_handler{};
+		functions::fix_vectors_t m_fix_vectors{};
+		functions::run_script_threads_t m_run_script_threads{};
+		rage::atArray<GtaThread*> *m_script_threads{};
 	};
 
 	struct Pointers : PointerData
