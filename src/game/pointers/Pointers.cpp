e84322885fa72ee9c569104ae38983e05439ea0a
Hexlane <deede3771@gmail.com>
2026-01-25 20:08:41 -0500

    Add new patterns for pointer scanning

--- src/game/pointers/Pointers.cpp
+++ src/game/pointers/Pointers.cpp
@@ -29,12 +29,33 @@ namespace NewBase
 			WndProc = ptr.As<WNDPROC>();
 		});
 
-		constexpr auto debugTrap = Pattern<"48 83 EC 28 33 C9 FF 15 ? ? ? ? 45 33 C9">("DebugTrap");
-		scanner.Add(debugTrap, [this](PointerCalculator ptr) {
-			BytePatch::Make(ptr.As<void*>(), std::to_array({ 0xC3, 0x90, 0x90, 0x90 }))->Apply();
-
-			UnhookWindowsHookEx(*ptr.Add(45).Rip().As<HHOOK*>());
+		constexpr auto PedfPTR = Pattern<"48 8B 05 ? ? ? ? 48 8B 48 08 48 85 C9 74 52 8B 81">("PF");
+		scanner.Add(PedfPTR, [this](PointerCalculator ptr) {
+			m_ped_factory = ptr.Add(3).Rip().As<CPedFactory**>();
 		});
+
+		constexpr auto NHPtr = Pattern<"48 8D 0D ? ? ? ? 48 8B 14 FA E8 ? ? ? ? 48 85 C0 75 0A">("NH");
+		scanner.Add(NHPtr, [this](PointerCalculator ptr) {
+			    m_init_native_tables = ptr.Sub(37).As<PVOID>();
+				m_native_registration_table = ptr.Add(3).Rip().As<rage::scrNativeRegistrationTable*>();
+				m_get_native_handler = ptr.Add(12).Rip().As<functions::get_native_handler_t>();
+		});
+
+		constexpr auto FVPtr = Pattern<"83 79 18 00 48 8B D1 74 4A FF 4A 18 48 63 4A 18 48 8D 41 04 48 8B 4C CA">("FV");
+		scanner.Add(FVPtr, [this](PointerCalculator ptr) {
+			   m_fix_vectors = ptr.As<functions::fix_vectors_t>();
+		});
+
+		constexpr auto STptr = Pattern<"45 33 F6 8B E9 85 C9 B8">("ST");
+		scanner.Add(STptr, [this](PointerCalculator ptr) {
+			    m_script_threads = ptr.Sub(4).Rip().Sub(8).As<decltype(m_script_threads)>();
+				m_run_script_threads = ptr.Sub(0x1F).As<functions::run_script_threads_t>();
+		});
+
+
+
+		
+	
 
 		if (!scanner.Scan())
 		{
