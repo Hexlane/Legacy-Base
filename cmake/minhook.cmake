ccda1b2d99f4d8988787e7a080201e4cafe0ce55
Hexlane <deede3771@gmail.com>
2026-01-25 21:21:15 -0500

    Update minhook GIT_TAG to a newer version

--- cmake/minhook.cmake
+++ cmake/minhook.cmake
@@ -6,7 +6,7 @@ message(STATUS "Setting up ${LIB_NAME}")
 FetchContent_Declare(
     ${LIB_NAME}
     GIT_REPOSITORY https://github.com/TsudaKageyu/minhook.git
-    GIT_TAG        f5485b8454544c2f034c78f8f127c1d03dea3636
+    GIT_TAG        1e9ad1eb42db11bfcb65461f687c656612d1b555
 	GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(${LIB_NAME})
