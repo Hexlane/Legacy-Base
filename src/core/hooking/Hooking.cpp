70cf79db2ca95f34cdf0d29744b53b633bc48ec5
Hexlane <deede3771@gmail.com>
2026-01-25 20:06:47 -0500

    Update Hooking.cpp

--- src/core/hooking/Hooking.cpp
+++ src/core/hooking/Hooking.cpp
@@ -1,13 +1,34 @@
 #include "Hooking.hpp"
-
+#include "game/Script/script.h"
 #include "BaseHook.hpp"
 #include "DetourHook.hpp"
 #include "VMTHook.hpp"
 #include "game/hooks/Hooks.hpp"
 #include "game/pointers/Pointers.hpp"
+#include "game/Script/script_mgr.h"
 
 namespace NewBase
 {
+	
+bool run_script_threads(std::uint32_t ops_to_execute)
+{
+    TRY_CLAUSE
+    {
+        if (g_Running)
+        {
+            g_script_mgr.tick();
+        }
+        return BaseHook::Get<run_script_threads, DetourHook<functions::run_script_threads_t>>()->Original()(ops_to_execute);
+    }
+     EXCEPT_CLAUSE
+    return false;
+}
+
+
+
+
+
+
 	Hooking::Hooking()
 	{
 		BaseHook::Add<Window::WndProc>(new DetourHook("WndProc", Pointers.WndProc, Window::WndProc));
@@ -16,6 +37,9 @@ namespace NewBase
 		swapchain->Hook(SwapChain::VMTPresentIdx, SwapChain::Present);
 		swapchain->Hook(SwapChain::VMTResizeBuffersIdx, SwapChain::ResizeBuffers);
 		BaseHook::Add<SwapChain::Present>(swapchain);
+       
+		BaseHook::Add<run_script_threads>(new DetourHook("Script hook", Pointers.m_run_script_threads, run_script_threads));
+
 	}
 
 	Hooking::~Hooking()
